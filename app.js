0be1c68f7f11bf2c207422b65762f17d3161e888
J404Simpson <jasonjamessimpson@gmail.com>
2025-10-14 14:46:56 +1300

    Add Scanbot

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     try {
       scanbotSDK = await ScanbotSDK.initialize({
         licenseKey: licenseKey,
-        enginePath: './vendor/scanbot-web-sdk/bin/',
+        enginePath: './scanbot/bin/',
         onInitialize: () => console.log('✅ Scanbot SDK initialized!'),
         onInitializeError: (error) => console.error('❌ Init failed:', error)
       });
